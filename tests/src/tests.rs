b8d5e7b090191a34e5957441e35052458c2c133b
shaojunda <shaojunda@gmail.com>
2025-10-29 14:10:39 +0800

    refactor: update Spillman Lock test to use SECP256K1 lock

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -9,6 +9,7 @@ use ckb_testtool::{
     ckb_types::{
         bytes::Bytes,
         core::{
+            ScriptHashType,
             TransactionBuilder,
             TransactionView
         }, packed::*, prelude::*},
@@ -18,6 +19,14 @@ use ckb_testtool::{
 const EMPTY_WITNESS_ARGS: [u8; 16] = [16, 0, 0, 0, 16, 0, 0, 0, 16, 0, 0, 0, 16, 0, 0, 0];
 const UNLOCK_TYPE_COMMITMENT: u8 = 0x00;
 const UNLOCK_TYPE_TIMEOUT: u8 = 0x01;
+
+// Mainnet/Testnet secp256k1_blake160_sighash_all code_hash
+const SECP256K1_CODE_HASH: [u8; 32] = [
+    0x9b, 0xd7, 0xe0, 0x6f, 0x3e, 0xcf, 0x4b, 0xe0,
+    0xf2, 0xfc, 0xd2, 0x18, 0x8b, 0x23, 0xf1, 0xb9,
+    0xfc, 0xc8, 0x8e, 0x5d, 0x4b, 0x65, 0xa8, 0x63,
+    0x7b, 0x17, 0x72, 0x3b, 0xbd, 0xa3, 0xcc, 0xe8,
+];
 
 // Include your tests here
 // See https://github.com/xxuejie/ckb-native-build-sample/blob/main/tests/src/tests.rs for more examples
@@ -31,7 +40,6 @@ fn test_spillman_lock_commitment_path() {
     let spillman_lock_bin: Bytes = loader.load_binary("spillman-lock");
     let auth_bin: Bytes = loader.load_binary("../../deps/auth");
     let spillman_lock_out_point = context.deploy_cell(spillman_lock_bin);
-    let always_success_out_point = context.deploy_cell(ALWAYS_SUCCESS.clone());
     let auth_out_point = context.deploy_cell(auth_bin);
 
     let mut generator = Generator::new();
@@ -63,12 +71,20 @@ fn test_spillman_lock_commitment_path() {
         .build_script(&spillman_lock_out_point, Bytes::from(args))
         .expect("script");
 
-    let user_lock_script = context
-        .build_script(&always_success_out_point, Bytes::new())
-        .expect("script");
-    let merchant_lock_script = context
-        .build_script(&always_success_out_point, Bytes::new())
-        .expect("script");
+    // Build lock scripts for user and merchant using mainnet secp256k1 code_hash
+    // Note: We manually construct these scripts instead of deploying secp256k1 binary
+    // because we only need to verify the output lock script structure, not execute it
+    let user_lock_script = Script::new_builder()
+        .code_hash(SECP256K1_CODE_HASH.pack())
+        .hash_type(ScriptHashType::Type.into())
+        .args(Bytes::from(user_pubkey_hash.as_ref().to_vec()).pack())
+        .build();
+
+    let merchant_lock_script = Script::new_builder()
+        .code_hash(SECP256K1_CODE_HASH.pack())
+        .hash_type(ScriptHashType::Type.into())
+        .args(Bytes::from(merchant_pubkey_hash.as_ref().to_vec()).pack())
+        .build();
 
     // prepare cell deps
     let spillman_lock_dep = CellDep::new_builder()
@@ -134,7 +150,6 @@ fn test_spillman_lock_commitment_path() {
         .verify_tx(&success_tx, 10_000_000)
         .expect("pass verification");
     println!("consume cycles: {}", cycles);
-
 
     // wrong user signature should fail verification
     let wrong_user_signature = [0u8; 65];
